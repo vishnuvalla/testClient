b4047e83a504e8ba68aa9ef982436dc64a25d3f5
vishnuvalla <vishnuvalla@gmail.com>
2019-05-03 02:07:36 -0500

    fix con

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql      = require('mysql');
 // https://github.com/mysqljs/mysql
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
   host     : 'localhost',
   user     : 'admin',
   password : 'DeepRisk&&',
